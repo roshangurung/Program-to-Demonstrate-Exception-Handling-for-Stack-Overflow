dc5500e06a79a56594278b2f24b3bdacc211ce10
roshangurung <r.gurung1992@gmail.com>
2019-12-10 21:22:59 -0800

    Initial commit

---
+++ Program to Demonstrate Exception Handling for Stack Overflow/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program////This C# program is used to demonstrate exception handling for stack overflow. Typically the StackOverflowException is triggered by a recursive method that creates a deep call stack. Using try and catch, an error message is displayed when the error occurs.
+/////////////////Runtime Test Cases///
+{
+    static void excep(int value)
+    {
+        Console.WriteLine(value);
+        excep(++value);
+    }
+
+    static void Main()
+    {
+        try
+        {
+            excep(0);
+        }
+        catch (StackOverflowException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
+////Output: Program terminated due to StackOverflow Exception.
